b56bf2426214ca675b2a69838d4e2a80a55cae51
SUNNY SAVITA <engsunny3365@gmail.com>
2023-02-22 12:00:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,5 +66,5 @@ def index():
         return render_template('index.html')
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
