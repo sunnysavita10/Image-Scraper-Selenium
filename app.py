ba752b222a34b4a69e2475029787e21563b5b364
SUNNY SAVITA <engsunny3365@gmail.com>
2023-02-22 13:07:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def index():
                                 # send a request to the image URL and save the image
                                 image_data = requests.get(image_url).content
                                 mydict={"Index":index,"Image":image_data}
-                                img_data.append(my_dict)
+                                img_data.append(mydict)
                                 with open(os.path.join(save_directory, f"{query}_{image_tags.index(image_tag)}.jpg"), "wb") as f:
                                     f.write(image_data)
                     client = pymongo.MongoClient("mongodb+srv://pwskills:pwskills@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
